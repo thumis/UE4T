78f06802c593e259fa46e021876acfaf2ffc5725
thumis <tkoolhaas@hotmail.nl>
2020-05-21 20:57:09 +0200

    SpinnyBoys

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,14 +1,39 @@
 
 
  // changing individual properties with code and using setInterval
- var rotationSpeed = 0.01;
+ var rotationSpeed = 0.25;
+    var rotationSlowDown = -0.0005;
+ var position = 0.0025;
+    var positionSlowDown = -0.004;
+
  var myOtherBox = document.getElementById('myOtherBox');
 
- function spin(){
- 	myOtherBox.object3D.rotation.x -= rotationSpeed;
+ function fidgetSpinner(){
+
+ 	myOtherBox.object3D.rotation.x += rotationSpeed;
      myOtherBox.object3D.rotation.y -= rotationSpeed;
      myOtherBox.object3D.rotation.z -= rotationSpeed;
-     console.log(myOtherBox.object3D.rotation.x);
+
+     myOtherBox.object3D.position.x += position;
+
+     console.log(
+     "rotation x: " + myOtherBox.object3D.rotation.x + "\n" +
+     "position x: " + myOtherBox.object3D.position.x + "\n" +
+     "Speed: "+  rotationSpeed);
+     if (rotationSpeed <= 0.001 ) {
+         rotationSpeed = 0.25;
+     }
+     else{
+         rotationSpeed += rotationSlowDown;
+     }
+     if(myOtherBox.object3D.position.x <= -10){
+         myOtherBox.object3D.position.x = 1;
+         position = 0.0025;
+     }
+     else{
+         position += positionSlowDown;
+     }
+     ;
  }
 
- setInterval(spin, 16); //equivalent to 60 fps
+ setInterval(fidgetSpinner, 16); //equivalent to 60 fps
